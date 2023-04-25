5dd3b00337e373bd142ae925f7630fc3e288fe07
parmisnaddaf <parmisnaddaf@yahoo.com>
2023-04-25 00:06:23 -0700

    cluster

--- main_after.py
+++ main_after.py
@@ -51,7 +51,7 @@ parser = argparse.ArgumentParser(description='Inductive')
 
 parser.add_argument('-e', type=int, dest="epoch_number", default=100, help="Number of Epochs")
 parser.add_argument('--model', type=str, default='KDD')
-parser.add_argument('--dataSet', type=str, default='cora')
+parser.add_argument('--dataSet', type=str, default='citeseer')
 parser.add_argument('--seed', type=int, default=123)
 parser.add_argument('-num_node', dest="num_node", default=-1, type=str,
                     help="the size of subgraph which is sampled; -1 means use the whole graph")
@@ -236,8 +236,8 @@ for i in sample_list:
     if single_link:
 
         adj_list_copy = copy.deepcopy(org_adj)
-        adj_list_copy[idd, neighbour_id] = 1  # find a test edge and set it to 0
-        adj_list_copy[neighbour_id, idd] = 1  # find a test edge and set it to 0
+        adj_list_copy[idd, neighbour_id] = 0  # find a test edge and set it to 0
+        adj_list_copy[neighbour_id, idd] = 0  # find a test edge and set it to 0
 
         targets.append(idd)
         targets.append(neighbour_id)
@@ -257,21 +257,19 @@ for i in sample_list:
 
 
     if multi_link:
-        # if disjoint_transductive_inductive:
-        #     adj_list_copy = copy.deepcopy(org_adj)
-        #     adj_list_copy[idd, testId] = 1
-        #     adj_list_copy[testId, idd] = 1
-        # else:
-        #     adj_list_copy[idd, :] = 1  
-        #     adj_list_copy[:, idd] = 1 
+        # if we want to set all target edges to 1
+        if disjoint_transductive_inductive: 
+            adj_list_copy_1 = copy.deepcopy(org_adj)
+            adj_list_copy_1[idd, testId] = 1
+            adj_list_copy_1[testId, idd] = 1
+        else:
+            adj_list_copy_1[idd, :] = 1  
+            adj_list_copy_1[:, idd] = 1 
         
-        # std_z_recog, m_z_recog, z_recog, re_adj_recog = run_network(features_kdd, adj_list_copy, inductive_pn, [], sampling_method,
-        #                                                             is_prior=False)
+        std_z_recog, m_z_recog, z_recog, re_adj_recog = run_network(features_kdd, adj_list_copy_1, inductive_pn, [], sampling_method,
+                                                                    is_prior=False)
 
         
-        adj_list_copy = copy.deepcopy(org_adj)
-        adj_list_copy[idd, :] = 0  # set all the neigbours to 0
-        adj_list_copy[:, idd] = 0  # set all the neigbours to 0
 
         true_multi_links = org_adj[idd].nonzero()
         false_multi_links = np.array(random.sample(list(np.nonzero(org_adj[idd] == 0)[0]), len(true_multi_links[0])))
@@ -280,16 +278,22 @@ for i in sample_list:
         target_list.extend([[idd, i] for i in list(false_multi_links)])
         target_list = np.array(target_list)
         
-        adj_list_copy_1 = copy.deepcopy(org_adj)
-        adj_list_copy_1[target_list[:,0], target_list[:,1]] = 1
-        adj_list_copy_1[target_list[:,1], target_list[:,0]] = 1
-        std_z_recog, m_z_recog, z_recog, re_adj_recog = run_network(features_kdd, adj_list_copy_1, inductive_pn, [], sampling_method,
-                                                                    is_prior=False)
+        # run rec to update q
+        # adj_list_copy_1 = copy.deepcopy(org_adj)
+        # adj_list_copy_1[target_list[:,0], target_list[:,1]] = 1 # set target edges to 1 
+        # adj_list_copy_1[target_list[:,1], target_list[:,0]] = 1 # set target edges to 1
+        # std_z_recog, m_z_recog, z_recog, re_adj_recog = run_network(features_kdd, adj_list_copy_1, inductive_pn, [], sampling_method,
+        #                                                             is_prior=False)
 
         targets = list(true_multi_links[0])
         targets.extend(list(false_multi_links))
         targets.append(idd)
-
+        
+        
+        # run prior
+        adj_list_copy = copy.deepcopy(org_adj)
+        adj_list_copy[idd, :] = 0  # set all the neigbours to 0
+        adj_list_copy[:, idd] = 0  # set all the neigbours to 0
         std_z_prior, m_z_prior, z_prior, re_adj_prior = run_network(features_kdd, adj_list_copy, inductive_pn,
                                                                     targets, sampling_method, is_prior=True)
 
@@ -396,7 +400,7 @@ if single_link:
     
 
     # only for A0 and A1
-    if sampling_method== "normalized":
+    if sampling_method == "normalized":
         xx = 0
         for test_neg_edge in test_neg_edges[:false_count]:
             if xx % 10 == 0:
